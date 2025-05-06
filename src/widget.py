47867fa27c1bf840ca4245b148c6327640bfb49e
Dariya Lavrova <lavrovads@yandex.ru>
2025-05-06 17:26:43 +0300

    fixes

---
+++ src/widget.py
@@ -0,0 +1,23 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+def mask_account_card (card_info: str, ) -> str:
+    """Функция принимает тип и номер карты или счета и возвращает строку с замаскированным номером"""
+
+    parts = card_info.split(" ")
+    card_number = parts[-1]
+    card_type = " ".join(parts[:-1])
+    if card_type == "Счет":
+        masked_number = get_mask_account(card_number)
+        return f"{card_type} {masked_number}"
+    else:
+        masked_number = get_mask_card_number(card_number)
+        return f"{card_type} {masked_number}"
+
+
+def get_date(date: str) -> str:
+    """Функция принимает на вход строку с датой в длинном формате и возвращает короткий вариант даты"""
+
+    year = date[0:4]
+    month = date[5:7]
+    day = date[8:10]
+    return f"{day}.{month}.{year}"
--- tests/test.py
+++ tests/test.py
@@ -20,3 +20,34 @@ def get_mask_account(account_number):
 account_number = ""
 masked_account = get_mask_account(account_number)
 print(masked_account)
+
+
+def mask_account_card (card_info: str, ) -> str:
+    """Функция принимает тип и номер карты или счета и возвращает строку с замаскированным номером"""
+
+    parts = card_info.split(" ")
+    card_number = parts[-1]
+    card_type = " ".join(parts[:-1])
+    if card_type == "Счет":
+        masked_number = get_mask_account(card_number)
+        return f"{card_type} {masked_number}"
+    else:
+        masked_number = get_mask_card_number(card_number)
+        return f"{card_type} {masked_number}"
+
+
+card_info = "Счет 64686473678894779589"
+masked_number = mask_account_card (card_info)
+print(masked_number)
+
+
+def get_date(date: str) -> str:
+    """Функция принимает на вход строку с датой в длинном формате и возвращает короткий вариант даты"""
+
+    year = date[0:4]
+    month = date[5:7]
+    day = date[8:10]
+    return f"{day}.{month}.{year}"
+
+date = "2024-03-11T02:26:18.671407"
+print(get_date(date))
