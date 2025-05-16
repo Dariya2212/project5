ac317cf346ab952e760c3d79bcf52cb62054b0ae
Dariya Lavrova <lavrovads@yandex.ru>
2025-05-16 17:57:54 +0300

    add processing.py

--- tests/test.py
+++ tests/test.py
@@ -54,3 +54,17 @@ def get_date(date: str) -> str:
 
 date = "2024-03-11T02:26:18.671407"
 print(get_date(date))
+
+
+def filter_by_state(bank_data: [dict], state="EXECUTED") -> [dict]:
+    '''Функция принимает на вход список словарей с данными о банковских операциях и параметр state, возвращает новый список, содержащий только те словари, у которых ключ state содержит переданное в функцию значение.'''
+
+    filtered_bank_data = []
+    for i in bank_data:
+        if i.get("state") == state:
+            filtered_bank_data.append(i)
+    return filtered_bank_data
+
+
+bank_data = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+print(filter_by_state(bank_data))
