e2873bc55ffb12f351dc894b2e0dad0edb6bbf35
Dariya Lavrova <lavrovads@yandex.ru>
2025-06-04 15:31:26 +0300

    add generators.py

---
+++ src/generators.py
@@ -0,0 +1,40 @@
+from typing import List, Dict, Any, Generator
+
+
+def filter_by_currency(transactions: List[Dict], currency_code="USD") -> Generator[dict, Any, None]:
+    """
+    Функция принимает на вход список словарей, представляющих транзакции
+    и возвращает итератор, который поочередно выдает транзакции
+    """
+    for transaction in transactions:
+        if ('operationAmount' in transaction and 'currency' in transaction['operationAmount']
+        and 'code' in transaction['operationAmount']['currency']):
+            if transaction['operationAmount']['currency']['code'] == currency_code:
+                yield transaction
+
+
+def transaction_descriptions(transactions: List[Dict]) -> Generator[dict, Any, None]:
+    """
+    Функция принимает список словарей с транзакциями
+    и возвращает описание каждой операции по очереди.
+    """
+
+    for transaction in transactions:
+        if "description" in transactions:
+            yield transaction.get("description")
+
+
+def card_number_generator (start: int, end: int) -> Generator[str, Any, None]:
+    """
+    Генератор выдает номера банковских карт в формате XXXX XXXX XXXX XXXX
+    """
+
+    for number in range(start, end + 1):
+        # Форматируем число с ведущими нулями до 16 цифр
+        card_num = f"{number:016d}"
+
+        # Разбиваем на группы по 4 цифры
+        formatted_card_number = ' '.join([card_num[i:i + 4] for i in range(0, 16, 4)])
+        yield formatted_card_number
+
+
