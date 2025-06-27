71ecb0c05d8f05dac57409ffb2e989335cec2d1c
Dariya Lavrova <lavrovads@yandex.ru>
2025-06-27 19:50:24 +0300

    add logs

--- src/masks.py
+++ src/masks.py
@@ -1,24 +1,46 @@
-def get_mask_card_number(card_number: str) -> str:
+import logging
+
+logger = logging.getLogger("masks")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("C:/Projects/PythonProject5/logs/masks.log", encoding="utf-8", mode="w")
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
+def get_mask_card_number(card_number: str) -> str | None:
     """Функция принимает на вход номер карты и возвращает ее маску"""
 
-    for i in card_number:
-        if i.isalpha():
+    try:
+        for i in card_number:
+            if i.isalpha():
+                logger.error("Введен номер карты, состоящий не из 16 цифр.")
+                return "Номер карты должен состоять из 16 цифр"
+        if len(card_number) != 16:
+            logger.error("Введен номер карты, состоящий не из 16 цифр.")
             return "Номер карты должен состоять из 16 цифр"
-    if len(card_number) != 16:
-        return "Номер карты должен состоять из 16 цифр"
-    else:
-        masked_card_number = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
-        return masked_card_number
+        else:
+            masked_card_number = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+            logger.info("Введен корректный номер карты.")
+            return masked_card_number
+    except Exception as ex:
+        logger.error(f"Произошла ошибка: {ex}.")
 
 
-def get_mask_account(account_number: str) -> str:
+def get_mask_account(account_number: str) -> str | None:
     """Функция принимает на вход номер счета и возвращает его маску"""
 
-    for i in account_number:
-        if i.isalpha():
+    try:
+        for i in account_number:
+            if i.isalpha():
+                logger.error("Введен некорректный номер счета.")
+                return "Номер счета должен состоять из 20 цифр"
+        if len(account_number) != 20:
+            logger.error("Введен некорректный номер счета.")
             return "Номер счета должен состоять из 20 цифр"
-    if len(account_number) != 20:
-        return "Номер счета должен состоять из 20 цифр"
-    else:
-        masked_account_number = f"**{account_number[-4:]}"
-        return masked_account_number
+        else:
+            logger.info("Введен корректный номер счета.")
+            masked_account_number = f"**{account_number[-4:]}"
+            return masked_account_number
+    except Exception as ex:
+        logger.error(f"Произошла ошибка: {ex}.")
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,27 @@
 import json
+import logging
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("C:/Projects/PythonProject5/logs/utils.log", encoding="utf-8", mode="w")
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def load_transactions(json_path):
+    """
+    Функция преобразует данные JSON-файла в список
+    """
+
     try:
+        logger.info("Преобразуем данные JSON-файла в список")
         with open(json_path, "r", encoding="utf-8") as f:
             operations = json.load(f)
     except (json.JSONDecodeError, TypeError, KeyError, ValueError):
+        logger.error("Произошла ошибка.")
+        return []
+    except FileNotFoundError:
+        logger.error("Файл не найден.")
         return []
     return operations
